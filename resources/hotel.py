00795f67b071f05fac608538e99e4a68c4d3646a
Leonardo Perrella <leonardo.perrella@yahoo.com.br>
2019-11-21 09:15:37 -0300

    First Commit

---
+++ resources/hotel.py
@@ -0,0 +1,57 @@
+from sqlite3 import IntegrityError
+from flask_restful import Resource, reqparse
+from models.hotel import HotelModel
+
+
+class Hoteis(Resource):
+    def get(self):
+        hoteis_all = []
+        hoteis = HotelModel.query.all()
+        for hotel in hoteis:
+            hoteis_all.append(hotel.json())
+
+        return {'hoteis': hoteis_all}
+
+
+class Hotel(Resource):
+    argumentos = reqparse.RequestParser()
+    argumentos.add_argument('nome')
+    argumentos.add_argument('estrelas')
+    argumentos.add_argument('diaria')
+    argumentos.add_argument('cidade')
+
+    def get(self, hotel_id):
+        hotel = HotelModel.find_hotel(hotel_id)
+        if not hotel:
+            return {'message': 'Hotel not found'}, 404
+        return hotel.json()
+
+    def post(self, hotel_id):
+
+        if HotelModel.find_hotel(hotel_id):
+            return {"message": f"Hotel id '{hotel_id}' already exixts."}, 400 #bad request
+
+        dados = self.argumentos.parse_args()
+        hotel = HotelModel(hotel_id, **dados)
+        hotel.save_hotel()
+        return hotel.json()
+
+    def put(self, hotel_id):
+
+        dados = self.argumentos.parse_args()
+
+        hotel = HotelModel(hotel_id, **dados)
+        
+        hotel_encontrado = HotelModel.find_hotel(hotel_id)
+        
+        if not hotel_encontrado:
+            hoteis.append(hotel)
+            return novo_hotel, 201  # created
+
+        hotel.update(hotel)
+        return hotel, 200 #ok
+
+    def delete(self, hotel_id):
+        global hoteis
+        hoteis = [hotel for hotel in hoteis if hotel['hotel_id'] != hotel_id]
+        return {'message': 'Hotel deleted'}, 200
